32d799e2188ab2ce964273170ac75fdaa6c342b9
Gabriel Hazout <gabrielhzt@Gabriels-MacBook-Pro.local>
2024-06-04 15:43:22 +0200

    Made the info page design

--- src/Account/Info/info.js
+++ src/Account/Info/info.js
@@ -1,9 +1,15 @@
 import React from "react";
+import './info.css';
 
 const Info = () => {
     return (
-        <div>
-            <h1>info</h1>
+        <div className="info">
+            <p>On this page, you can view your current username and email address. To modify this information, press the "Edit Profile" button. Then, fill in the fields with the new information and press "Edit Profile" again to save the changes.</p>
+            <div>
+                <p>Email:</p>
+                <p>Password:</p>
+                <button className="edit">Edit</button>
+            </div>
         </div>
     )
 }
