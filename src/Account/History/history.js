fdb9f2a792c470467b33b9ca49e55cdddbd88f75
Gabriel Hazout <gabrielhzt@Gabriels-MacBook-Pro.local>
2024-06-04 17:35:27 +0200

    Made the order history page design

--- src/Account/History/history.js
+++ src/Account/History/history.js
@@ -1,9 +1,46 @@
 import React from "react";
+import './history.css';
 
 const History = () => {
     return (
-        <div>
-            <h1>History</h1>
+        <div className="info">
+            <p>On this page, find a summary of all your past orders, including details such as the order number, and total amount of each purchase. It's a convenient way to keep track of your previous purchases and reorder if needed.</p>
+            <div>
+                <div>
+                    <div className="top-history">
+                        <h2>Order 1</h2>
+                        <p>Total Amount: $1000</p>
+                    </div>
+                    <div className="gap">
+                        <div className='order-product'>
+                            <div className='info-product'>
+                                <img src="https://www.vanmoof.com/sites/default/files/2023-03/PDP-D-Carousel-S5-Light-01_0.jpg" alt="" className='img-history' />
+                                <div className='text-product'>
+                                    <div>
+                                        <h3>VB Pulse</h3>
+                                        <p>$500</p>
+                                    </div>
+                                    <div></div>
+                                </div>
+                            </div>
+                            <p>Quantity: 1</p>
+                        </div>
+                        <div className='order-product'>
+                            <div className='info-product'>
+                                <img src="https://www.vanmoof.com/sites/default/files/2023-03/PDP-D-Carousel-S5-Light-01_0.jpg" alt="" className='img-history' />
+                                <div className='text-product'>
+                                    <div>
+                                        <h3>VB Pulse</h3>
+                                        <p>$500</p>
+                                    </div>
+                                    <div></div>
+                                </div>
+                            </div>
+                            <p>Quantity: 1</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
