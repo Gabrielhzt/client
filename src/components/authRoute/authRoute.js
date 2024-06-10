29afcd80b48c6c82915d0166085e22d677a66b3d
Gabriel Hazout <gabrielhzt@Gabriels-MacBook-Pro.local>
2024-06-10 12:14:20 +0200

    change the redirect path for the authpath

--- src/components/authRoute/authRoute.js
+++ src/components/authRoute/authRoute.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navigate } from 'react-router';
+import { fetchUserInfo } from '../../features/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 const AuthRoute = ({ loadingUser, user, errorUser }) => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchUserInfo())
+    }, [])
 
     if (loadingUser) {
         return <div>Loading...</div>;
     }
 
     if (user.full_name) {
-        return <Navigate to="/cart" />;
+        return <Navigate to='/account/personal-info' />;
     }
 
     return <Outlet />;
