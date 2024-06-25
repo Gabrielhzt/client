08379721be0a942db3cfe43dd4f42b17afccf5e5
Gabriel Hazout <gabrielhzt@Gabriels-MacBook-Pro.local>
2024-06-25 17:00:45 +0200

    Made some change on the paymentForm

--- src/Payment/paymentForm.js
+++ src/Payment/paymentForm.js
@@ -7,7 +7,7 @@ const PaymentForm = ({ cart }) => {
     const stripe = useStripe();
     const elements = useElements();
     const dispatch = useDispatch();
-    const [error, setError] = useState('?')
+    const [error, setError] = useState(null); // Utilisation de null pour les erreurs par défaut
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -18,45 +18,50 @@ const PaymentForm = ({ cart }) => {
 
         const address = elements.getElement(AddressElement);
 
-        const { error } = await stripe.confirmPayment({
-            elements,
-            confirmParams: {
-                redirect: 'if_required',
-                shipping: {
-                    name: address.name,
-                    address: {
-                        line1: address.line1,
-                        line2: address.line2,
-                        city: address.city,
-                        state: address.state,
-                        postal_code: address.postal_code,
-                        country: address.country,
+        try {
+            const { error } = await stripe.confirmPayment({
+                elements,
+                confirmParams: {
+                    redirect: 'if_required',
+                    shipping: {
+                        name: address.name,
+                        address: {
+                            line1: address.line1,
+                            line2: address.line2,
+                            city: address.city,
+                            state: address.state,
+                            postal_code: address.postal_code,
+                            country: address.country,
+                        },
                     },
                 },
-            },
-        })
-        .then(({ error }) => {
+            });
+
             if (!error) {
                 console.log('Paiement confirmé avec succès!');
                 dispatch(validateCart());
                 window.location.href = 'https://voltbike.vercel.app/payment-success';
             } else {
                 console.error('Erreur lors de la confirmation du paiement:', error);
+                setError(error.message); // Mise à jour de l'état de l'erreur pour affichage si nécessaire
             }
-        })
-        .catch(error => {
+        } catch (error) {
             console.error('Erreur lors de la confirmation du paiement:', error);
-        });
+            setError(error.message); // Mise à jour de l'état de l'erreur pour affichage si nécessaire
+        }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <PaymentElement />
-            <AddressElement options={{ mode: 'shipping' }} />
-            <button type="submit" disabled={!stripe} className='btn2'>
-                Payer
-            </button>
-        </form>
+        <div>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <PaymentElement />
+                <AddressElement options={{ mode: 'shipping' }} />
+                <button type="submit" disabled={!stripe} className='btn2'>
+                    Payer
+                </button>
+            </form>
+        </div>
     );
 };
 
